2515f7d8310f024ac995880bef10284191fc9737
James Wolff <james.wolff0@gmail.com>
2023-02-27 22:57:57 +0000

    Add World Struct

--- src/world.rs
+++ src/world.rs
@@ -1,4 +1,36 @@
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
 pub mod campaign;
 pub mod location;
 pub mod map;
 pub mod npc;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct World {
+    pub id: String,
+    pub name: String,
+}
+
+impl World {
+    pub fn new(name: String) -> Self {
+        Self {
+            id: Uuid::new_v4().to_string(),
+            name,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct WorldRequest {
+    pub name: Option<String>,
+}
+
+impl WorldRequest {
+    pub fn to_world(&self) -> Option<World> {
+        match &self.name {
+            Some(name) => Some(World::new(name.to_string())),
+            None => None,
+        }
+    }
+}
