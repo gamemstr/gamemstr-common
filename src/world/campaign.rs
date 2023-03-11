ea2d9ac989ddfb78f448ee7ee3fa53feda5aa7ae
James Wolff <james.wolff0@gmail.com>
2023-03-11 22:22:39 +0000

    Begin campaign implementation

--- src/world/campaign.rs
+++ src/world/campaign.rs
@@ -1,1 +1,45 @@
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
 pub mod player;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Campaign {
+    pub id: String,
+    pub name: String,
+    pub description: String,
+    pub world_id: String,
+    pub players: Vec<player::Player>,
+}
+
+impl Campaign {
+    pub fn new(name: String, description: String, world_id: String) -> Self {
+        Self {
+            id: Uuid::new_v4().to_string(),
+            name,
+            description,
+            world_id,
+            players: Vec::new(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CampaignRequest {
+    pub name: Option<String>,
+    pub description: Option<String>,
+    pub world_id: Option<String>,
+}
+
+impl CampaignRequest {
+    pub fn to_campaign(&self) -> Option<Campaign> {
+        match &self.name {
+            Some(name) => Some(Campaign::new(
+                name.to_string(),
+                self.description.clone().expect("no description provided"),
+                self.world_id.clone().expect("no world_id provided"),
+            )),
+            None => None,
+        }
+    }
+}
