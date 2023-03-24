8e66200b25aae5ec200ba1a73e8a81b1692a48c6
James Wolff <james.wolff0@gmail.com>
2023-03-24 10:15:05 -0500

    impl Clone on Monster type

--- src/monster.rs
+++ src/monster.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 
 use crate::creature::Creature;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Monster {
     pub creature: Creature,
 }
@@ -15,7 +15,7 @@ impl Monster {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum MonsterType {
     Aberration,
     Beast,
