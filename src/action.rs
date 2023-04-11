70d42d9b6a32aa7f07e017312bf498cf8397c37f
James Wolff <james.wolff0@gmail.com>
2023-04-10 21:59:02 -0500

    impl fmt::Display on Action

--- src/action.rs
+++ src/action.rs
@@ -1,14 +1,11 @@
+use core::fmt;
+
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 use self::attack::Attack;
 
 pub mod attack;
-
-#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
-pub enum ActionType {
-    Attack(Attack),
-}
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct Action {
@@ -21,6 +18,19 @@ impl Action {
         Self {
             id: Uuid::new_v4().to_string(),
             action,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub enum ActionType {
+    Attack(Attack),
+}
+
+impl fmt::Display for ActionType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match &self {
+            ActionType::Attack(x) => write!(f, "{}", x.to_string()),
         }
     }
 }
