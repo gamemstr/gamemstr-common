90d55d14b282fc5da4f32a87cbdd6fb0ef78d3f8
James Wolff <james.wolff0@gmail.com>
2024-02-13 16:14:39 +0000

    Refactor Creature struct and add new methods
    Remove public members from the struct and add getters/setters, do not allow id to be an empty string and remove the ability to set it after creating the instance, move MonsterType to creature.rs

--- src/creature.rs
+++ src/creature.rs
@@ -2,38 +2,257 @@ use core::fmt;
 
 use serde::{Deserialize, Serialize};
 
-use crate::{
-    action::Action, Alignment, ConditionType, DamageType, Die, DieStat,
-    OtherAttribute,
-};
-
-pub mod monster;
-pub mod npc;
+use crate::{action::Action, Alignment, ConditionType, DamageType, Die, DieStat, OtherAttribute};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Creature {
-    pub id: String,
-    pub name: String,
-    pub creature_type: CreatureType,
-    pub alignment: Alignment,
-    pub armor_class: i32,
-    pub health_points: Health,
-    pub speed: MovementSpeed,
-    pub stats: Vec<Stat>,
-    pub saving_throws: Option<Vec<Stat>>,
-    pub damage_resistances: Option<Vec<DamageType>>,
-    pub damage_immunities: Option<Vec<DamageType>>,
-    pub damage_vulnerabilities: Option<Vec<DamageType>>,
-    pub condition_immunities: Option<Vec<ConditionType>>,
-    pub skills: Option<Vec<Skill>>,
-    pub senses: Option<Vec<Sense>>,
-    pub languages: Option<Vec<Language>>,
-    pub challenge_rating: String,
-    pub racial_traits: Option<Vec<RacialTrait>>,
-    pub description: Option<String>,
-    pub actions: Option<Vec<Action>>,
-    pub lair: Option<Lair>,
-    pub others: Option<Vec<OtherAttribute>>,
+    id: String,
+    name: String,
+    creature_type: CreatureType,
+    alignment: Alignment,
+    armor_class: i32,
+    health_points: Health,
+    speed: MovementSpeed,
+    stats: Vec<Stat>,
+    saving_throws: Option<Vec<Stat>>,
+    damage_resistances: Option<Vec<DamageType>>,
+    damage_immunities: Option<Vec<DamageType>>,
+    damage_vulnerabilities: Option<Vec<DamageType>>,
+    condition_immunities: Option<Vec<ConditionType>>,
+    skills: Option<Vec<Skill>>,
+    senses: Option<Vec<Sense>>,
+    languages: Option<Vec<Language>>,
+    challenge_rating: String,
+    racial_traits: Option<Vec<RacialTrait>>,
+    description: Option<String>,
+    actions: Option<Vec<Action>>,
+    lair: Option<Lair>,
+    others: Option<Vec<OtherAttribute>>,
+}
+
+impl Creature {
+    pub fn new(
+        id: String,
+        name: String,
+        creature_type: CreatureType,
+        alignment: Alignment,
+        armor_class: i32,
+        health_points: Health,
+        speed: MovementSpeed,
+        stats: Vec<Stat>,
+        saving_throws: Option<Vec<Stat>>,
+        damage_resistances: Option<Vec<DamageType>>,
+        damage_immunities: Option<Vec<DamageType>>,
+        damage_vulnerabilities: Option<Vec<DamageType>>,
+        condition_immunities: Option<Vec<ConditionType>>,
+        skills: Option<Vec<Skill>>,
+        senses: Option<Vec<Sense>>,
+        languages: Option<Vec<Language>>,
+        challenge_rating: String,
+        racial_traits: Option<Vec<RacialTrait>>,
+        description: Option<String>,
+        actions: Option<Vec<Action>>,
+        lair: Option<Lair>,
+        others: Option<Vec<OtherAttribute>>,
+    ) -> Self {
+        assert_ne!(id, "", "Creature ID cannot be empty");
+        Self {
+            id,
+            name,
+            creature_type,
+            alignment,
+            armor_class,
+            health_points,
+            speed,
+            stats,
+            saving_throws,
+            damage_resistances,
+            damage_immunities,
+            damage_vulnerabilities,
+            condition_immunities,
+            skills,
+            senses,
+            languages,
+            challenge_rating,
+            racial_traits,
+            description,
+            actions,
+            lair,
+            others,
+        }
+    }
+
+    pub fn id(&self) -> &str {
+        self.id.as_str()
+    }
+
+    pub fn name(&self) -> &str {
+        self.name.as_str()
+    }
+
+    pub fn creature_type(&self) -> &CreatureType {
+        &self.creature_type
+    }
+
+    pub fn alignment(&self) -> &Alignment {
+        &self.alignment
+    }
+
+    pub fn armor_class(&self) -> &i32 {
+        &self.armor_class
+    }
+
+    pub fn health_points(&self) -> &Health {
+        &self.health_points
+    }
+
+    pub fn speed(&self) -> &MovementSpeed {
+        &self.speed
+    }
+
+    pub fn stats(&self) -> &Vec<Stat> {
+        &self.stats
+    }
+
+    pub fn saving_throws(&self) -> Option<&Vec<Stat>> {
+        self.saving_throws.as_ref()
+    }
+
+    pub fn damage_resistances(&self) -> Option<&Vec<DamageType>> {
+        self.damage_resistances.as_ref()
+    }
+
+    pub fn damage_immunities(&self) -> Option<&Vec<DamageType>> {
+        self.damage_immunities.as_ref()
+    }
+
+    pub fn damage_vulnerabilities(&self) -> Option<&Vec<DamageType>> {
+        self.damage_vulnerabilities.as_ref()
+    }
+
+    pub fn condition_immunities(&self) -> Option<&Vec<ConditionType>> {
+        self.condition_immunities.as_ref()
+    }
+
+    pub fn skills(&self) -> Option<&Vec<Skill>> {
+        self.skills.as_ref()
+    }
+
+    pub fn senses(&self) -> Option<&Vec<Sense>> {
+        self.senses.as_ref()
+    }
+
+    pub fn languages(&self) -> Option<&Vec<Language>> {
+        self.languages.as_ref()
+    }
+
+    pub fn challenge_rating(&self) -> &str {
+        self.challenge_rating.as_str()
+    }
+
+    pub fn racial_traits(&self) -> Option<&Vec<RacialTrait>> {
+        self.racial_traits.as_ref()
+    }
+
+    pub fn description(&self) -> Option<&str> {
+        self.description.as_ref().map(|s| s.as_str())
+    }
+
+    pub fn actions(&self) -> Option<&Vec<Action>> {
+        self.actions.as_ref()
+    }
+
+    pub fn lair(&self) -> Option<&Lair> {
+        self.lair.as_ref()
+    }
+
+    pub fn others(&self) -> Option<&Vec<OtherAttribute>> {
+        self.others.as_ref()
+    }
+
+    pub fn set_name(&mut self, name: String) {
+        self.name = name;
+    }
+
+    pub fn set_creature_type(&mut self, creature_type: CreatureType) {
+        self.creature_type = creature_type;
+    }
+
+    pub fn set_alignment(&mut self, alignment: Alignment) {
+        self.alignment = alignment;
+    }
+
+    pub fn set_armor_class(&mut self, armor_class: i32) {
+        self.armor_class = armor_class;
+    }
+
+    pub fn set_health_points(&mut self, health_points: Health) {
+        self.health_points = health_points;
+    }
+
+    pub fn set_speed(&mut self, speed: MovementSpeed) {
+        self.speed = speed;
+    }
+
+    pub fn set_stats(&mut self, stats: Vec<Stat>) {
+        self.stats = stats;
+    }
+
+    pub fn set_saving_throws(&mut self, saving_throws: Option<Vec<Stat>>) {
+        self.saving_throws = saving_throws;
+    }
+
+    pub fn set_damage_resistances(&mut self, damage_resistances: Option<Vec<DamageType>>) {
+        self.damage_resistances = damage_resistances;
+    }
+
+    pub fn set_damage_immunities(&mut self, damage_immunities: Option<Vec<DamageType>>) {
+        self.damage_immunities = damage_immunities;
+    }
+
+    pub fn set_damage_vulnerabilities(&mut self, damage_vulnerabilities: Option<Vec<DamageType>>) {
+        self.damage_vulnerabilities = damage_vulnerabilities;
+    }
+
+    pub fn set_condition_immunities(&mut self, condition_immunities: Option<Vec<ConditionType>>) {
+        self.condition_immunities = condition_immunities;
+    }
+
+    pub fn set_skills(&mut self, skills: Option<Vec<Skill>>) {
+        self.skills = skills;
+    }
+
+    pub fn set_senses(&mut self, senses: Option<Vec<Sense>>) {
+        self.senses = senses;
+    }
+
+    pub fn set_languages(&mut self, languages: Option<Vec<Language>>) {
+        self.languages = languages;
+    }
+
+    pub fn set_challenge_rating(&mut self, challenge_rating: String) {
+        self.challenge_rating = challenge_rating;
+    }
+
+    pub fn set_racial_traits(&mut self, racial_traits: Option<Vec<RacialTrait>>) {
+        self.racial_traits = racial_traits;
+    }
+
+    pub fn set_description(&mut self, description: Option<String>) {
+        self.description = description;
+    }
+
+    pub fn set_actions(&mut self, actions: Option<Vec<Action>>) {
+        self.actions = actions;
+    }
+
+    pub fn set_lair(&mut self, lair: Option<Lair>) {
+        self.lair = lair;
+    }
+
+    pub fn set_others(&mut self, others: Option<Vec<OtherAttribute>>) {
+        self.others = others;
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -250,9 +469,9 @@ impl fmt::Display for Language {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub enum CreatureType {
-    Monster(monster::MonsterType),
+    Monster(MonsterType),
     Player,
     NPC,
 }
@@ -275,4 +494,28 @@ pub struct Lair {
 pub struct Paragraph {
     pub paragraph: String,
     pub bullet: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub enum MonsterType {
+    Aberration,
+    Beast,
+    Celestial,
+    Construct,
+    Dragon,
+    Elemental,
+    Fey,
+    Fiend,
+    Giant,
+    Humanoid,
+    Monstrosity,
+    Ooze,
+    Plant,
+    Undead,
+}
+
+impl fmt::Display for MonsterType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
 }
