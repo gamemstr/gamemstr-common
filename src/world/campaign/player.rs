0edc851ef8c552561f23e6727eae00bc1497d324
James Wolff <james.wolff0@gmail.com>
2024-02-13 16:16:06 +0000

    Refactor Player struct and add getter/setter methods, shadow creature getters into player

--- src/world/campaign/player.rs
+++ src/world/campaign/player.rs
@@ -2,18 +2,28 @@ use core::fmt;
 
 use serde::{Deserialize, Serialize};
 
-use crate::{creature::Creature, item::Item, spell::Spell, world::campaign::Campaign};
+use crate::{
+    action::Action,
+    creature::{
+        Creature, CreatureType, Health, Lair, Language, MovementSpeed, RacialTrait, Sense, Skill,
+        Stat,
+    },
+    item::Item,
+    spell::Spell,
+    world::campaign::Campaign,
+    Alignment, ConditionType, DamageType, OtherAttribute,
+};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Player {
-    pub creature: Creature,
-    pub race: Race,
-    pub class: Class,
-    pub level: u8,
-    pub experience: u32,
-    pub inventory: Vec<Item>,
-    pub spells: Vec<Spell>,
-    pub campaign: Campaign,
+    creature: Creature,
+    race: Race,
+    class: Class,
+    level: u8,
+    experience: u32,
+    inventory: Vec<Item>,
+    spells: Vec<Spell>,
+    campaign: Campaign,
 }
 
 impl Player {
@@ -25,6 +35,7 @@ impl Player {
         experience: u32,
         campaign: Campaign,
     ) -> Self {
+        assert_eq!(creature.creature_type(), &CreatureType::Player);
         Self {
             creature,
             race,
@@ -35,6 +46,154 @@ impl Player {
             spells: Vec::new(),
             campaign,
         }
+    }
+
+    pub fn race(&self) -> &Race {
+        &self.race
+    }
+
+    pub fn class(&self) -> &Class {
+        &self.class
+    }
+
+    pub fn level(&self) -> &u8 {
+        &self.level
+    }
+
+    pub fn experience(&self) -> &u32 {
+        &self.experience
+    }
+
+    pub fn campaign(&self) -> &Campaign {
+        &self.campaign
+    }
+
+    pub fn inventory(&self) -> &Vec<Item> {
+        &self.inventory
+    }
+
+    pub fn spells(&self) -> &Vec<Spell> {
+        &self.spells
+    }
+
+    pub fn add_item(&mut self, item: Item) {
+        self.inventory.push(item);
+    }
+
+    pub fn remove_item(&mut self, item: Item) {
+        if let Some(index) = self.inventory.iter().position(|i| i == &item) {
+            self.inventory.remove(index);
+        }
+    }
+
+    pub fn add_spell(&mut self, spell: Spell) {
+        self.spells.push(spell);
+    }
+
+    pub fn remove_spell(&mut self, spell: Spell) {
+        if let Some(index) = self.spells.iter().position(|s| s == &spell) {
+            self.spells.remove(index);
+        }
+    }
+
+    pub fn set_experience(&mut self, experience: u32) {
+        self.experience = experience;
+    }
+
+    pub fn set_level(&mut self, level: u8) {
+        self.level = level;
+    }
+
+    pub fn set_campaign(&mut self, campaign: Campaign) {
+        self.campaign = campaign;
+    }
+
+    pub fn id(&self) -> &str {
+        self.creature.id()
+    }
+
+    pub fn name(&self) -> &str {
+        self.creature.name()
+    }
+
+    pub fn creature_type(&self) -> &CreatureType {
+        self.creature.creature_type()
+    }
+
+    pub fn alignment(&self) -> &Alignment {
+        self.creature.alignment()
+    }
+
+    pub fn armor_class(&self) -> &i32 {
+        self.creature.armor_class()
+    }
+
+    pub fn hit_points(&self) -> &Health {
+        self.creature.health_points()
+    }
+
+    pub fn speed(&self) -> &MovementSpeed {
+        self.creature.speed()
+    }
+
+    pub fn stats(&self) -> &Vec<Stat> {
+        self.creature.stats()
+    }
+
+    pub fn saving_throws(&self) -> Option<&Vec<Stat>> {
+        self.creature.saving_throws()
+    }
+
+    pub fn damage_resistances(&self) -> Option<&Vec<DamageType>> {
+        self.creature.damage_resistances()
+    }
+
+    pub fn damage_immunities(&self) -> Option<&Vec<DamageType>> {
+        self.creature.damage_immunities()
+    }
+
+    pub fn damage_vulnerabilities(&self) -> Option<&Vec<DamageType>> {
+        self.creature.damage_vulnerabilities()
+    }
+
+    pub fn condition_immunities(&self) -> Option<&Vec<ConditionType>> {
+        self.creature.condition_immunities()
+    }
+
+    pub fn skills(&self) -> Option<&Vec<Skill>> {
+        self.creature.skills()
+    }
+
+    pub fn senses(&self) -> Option<&Vec<Sense>> {
+        self.creature.senses()
+    }
+
+    pub fn languages(&self) -> Option<&Vec<Language>> {
+        self.creature.languages()
+    }
+
+    pub fn challenge_rating(&self) -> &str {
+        self.creature.challenge_rating()
+    }
+
+    pub fn racial_traits(&self) -> Option<&Vec<RacialTrait>> {
+        self.creature.racial_traits()
+    }
+
+    pub fn description(&self) -> Option<&str> {
+        self.creature.description()
+    }
+
+    pub fn actions(&self) -> Option<&Vec<Action>> {
+        self.creature.actions()
+    }
+
+    pub fn lair(&self) -> Option<&Lair> {
+        None
+    }
+
+    pub fn others(&self) -> Option<&Vec<OtherAttribute>> {
+        self.creature.others()
     }
 }
 
